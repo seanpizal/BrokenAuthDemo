db29b65dd81c017762882e8f53db39bf80dbe856
Jorge Flores <jorge.andres.flores.zepeda@gmail.com>
2025-02-09 12:51:17 -0500

    Deployment

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 # Remove previous versions
 # rm -rf /opt/brokenauthdemo/*
+#!/bin/bash
+
+# Update the package manager
+sudo yum update -y
+
+# Install the .NET 8.0 runtime
+# Add the Microsoft package signing key
+sudo rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
+
+# Install the .NET 8.0 runtime
+sudo yum install -y dotnet-sdk-8.0
+
+# Verify the installation
+dotnet --version
