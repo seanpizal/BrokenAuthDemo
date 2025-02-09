13c3d7203261558796a499bedab08871965315dd
Jorge Flores <jorge.andres.flores.zepeda@gmail.com>
2025-02-09 12:57:37 -0500

    Deployment

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,1 +1,13 @@
-pkill -f "dotnet BrokenAuthDemo.dll"
+#!/bin/bash
+
+# Check if the PID file exists
+if [ -f /var/run/brokenauthdemo.pid ]; then
+  # Read the PID from the file
+  PID=$(cat /var/run/brokenauthdemo.pid)
+  
+  # Kill the process
+  kill $PID
+  
+  # Remove the PID file
+  rm /var/run/brokenauthdemo.pid
+fi
